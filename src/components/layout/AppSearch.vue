9a2be8fd5f8a7a6a556e6f7b6c3cd6f9e4728d13
Teerachai Laothong <tlaothong@gmail.com>
2023-06-07 11:44:48 +0700

    Added management bar for a page.

--- src/components/layout/AppSearch.vue
+++ src/components/layout/AppSearch.vue
@@ -4,17 +4,11 @@
     :class="{ 'hidden md:block md:pl-2': !mobile, 'md:hidden mb-2': mobile }">
     <label for="topbar-search"
       class="sr-only">Search</label>
-    <div class="relative" :class="{ 'md:w-96': !mobile }">
+    <div class="relative"
+      :class="{ 'md:w-96': !mobile }">
       <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z">
-          </path>
-        </svg>
+        <Icon class="w-5 h-5 text-gray-500 dark:text-gray-400"
+          name="mdi:magnify"></Icon>
       </div>
       <input type="text"
         name="email"
---
+++ src/components/ui/ManageBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="order-2 md:order-3">
+    <div class="flex flex-col items-center pb-3 md:flex-row">
+      <form class="relative mb-4 w-full md:mr-4 md:max-w-sm md:mb-0">
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <Icon class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            name="mdi:magnify"></Icon>
+        </div>
+        <input type="search"
+          class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+          placeholder="Search">
+      </form>
+      <div class="grid grid-cols-2 gap-4 items-center w-full md:gap-0 md:flex md:w-auto">
+        <slot></slot>
+      </div>
+    </div>
+  </nav>
+</template>
