fc58d74bb54f8b607d10e467001808da43b68aed
Rocky0191 <107449941+Rocky0191@users.noreply.github.com>
2023-06-08 16:04:37 +0700

    update Page report

--- src/pages/report.vue
+++ src/pages/report.vue
@@ -35,6 +35,41 @@
 
 
     <div class="grid grid-cols-4 gap-2">
+      <div>
+        <a href="#"
+          class="flex justify-between block max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <Icon class="w-6 h-6" name="ph:plus-fill"></Icon>
+            490,093
+          </h5>
+          <h1 class="text-3 font-bold tracking-tight text-green-600 dark:text-white">
+            +10% <Icon class="w-6 h-6" name="ph:arrow-up-bold"></Icon>
+          </h1>
+        </a>
+
+        <a href="#"
+          class="flex justify-between block max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <Icon class="w-6 h-6" name="ph:minus-fill"></Icon>
+            385,872
+          </h5>
+          <h5 class=" mb-2 text-3 font-bold tracking-tight text-red-600 dark:text-red">
+            -5% <Icon class="w-6 h-6" name="ph:arrow-down-bold"></Icon>
+          </h5>
+        </a>
+
+        <a href="#"
+          class="flex justify-between block max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <Icon class="w-6 h-6" name="icon-park-solid:add-subtract"></Icon>
+            104,221
+          </h5>
+          <h5 class="text-3 font-bold tracking-tight text-green-600 dark:text-red">
+            +23% <Icon class="w-6 h-6" name="ph:arrow-up-bold"></Icon>
+          </h5>
+        </a>
+      </div>
+
       <div class="col-span-3">
         <o-table :data="data" checkable :checkbox-position="checkboxPosition">
           <o-table-column v-for="column in columns" v-bind="column" #default="{ row }">
@@ -42,18 +77,8 @@
           </o-table-column>
         </o-table>
       </div>
-
-      <div>
-        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a>
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5>
-          </a>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">รายรับ <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">490093</span></p>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">รายจ่าย <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">385872</span></p>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">กำไร/ขาดทุน <span class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">104221</span></p>
-        </div>
-      </div>
     </div>
+
 
 
 
