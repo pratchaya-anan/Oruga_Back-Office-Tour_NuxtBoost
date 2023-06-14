675891ad65e3bd9e93f69db0fa06e2b39813f2d6
zxc2040 <107449746+zxc2040@users.noreply.github.com>
2023-06-14 15:37:43 +0700

    Update oruga.ts

--- src/plugins/oruga.ts
+++ src/plugins/oruga.ts
@@ -55,15 +55,13 @@ export default defineNuxtPlugin((nuxtApp) => {
       },
       steps: {
         rootClass: "w-full text-sm ",
-        stepsClass: "flex w-full text-sm font-medium text-gray-500 dark:text-gray-400 sm:text-base",
         animatedClass: " items-center justify-around w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base",
-        // stepContentClass: "hidden",
-        // stepContentClass: "hidden",
-
-        itemHeaderClass: "flex w-full items-center dark:text-blue-500",
-        itemHeaderActiveClass: " w-full items-center text-blue-600 dark:text-blue-500 ",
-        itemHeaderPreviousClass: "w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800 ",
-
+        stepsClass: "flex justify-between items-center w-full text-sm font-medium text-gray-500 dark:text-gray-400 sm:text-base",
+        itemHeaderClass: " w-full items-center dark:text-blue-500",
+        itemHeaderActiveClass: " w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800",
+        itemHeaderPreviousClass: " w-full items-center text-blue-600 dark:text-blue-500  after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800",
+        stepNavigationClass: "flex justify-between",
+        stepDividerClass : "flex justify-between",
       },
       radio: {
         rootClass: 'flex text-sm font-medium text-center items-center mb-1',
