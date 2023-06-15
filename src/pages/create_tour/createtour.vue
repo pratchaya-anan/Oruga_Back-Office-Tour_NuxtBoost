14f2b68cdcde96f38389ba676922f86076f66f78
jane99j <jaynerrch@gmail.com>
2023-06-15 15:01:19 +0700

    createtour

--- src/pages/create_tour/createtour.vue
+++ src/pages/create_tour/createtour.vue
@@ -69,9 +69,8 @@
         </UiCard>
       </section>
     </o-step-item>
-    <o-step-item step="2" label="ข้อมูลไกด์" :clickable="true" icon="user"></o-step-item>
-    <o-step-item step="3" label="ข้อมูลโรงแรม" :clickable="true" icon="user-plus">
-      <!-- 3 โรงแรม -->
+    <o-step-item step="2" label="ข้อมูลไกด์" :clickable="true" icon="user">
+      <!-- 2 ไกด์ -->
       <section class="h-m-screen">
         <div class="grid grid-cols-1 xl:grid-cols-3 xl:gap-4">
           <div class="col-span-full xl:col-auto mb-4">
@@ -80,6 +79,7 @@
 
                 <div>
                   <h2 class="text-xl font-bold dark:text-white">
+                    <Icon class="w-6 h-6" name="mdi:airplane-marker"></Icon>
                     ทัวร์เกาะเสม็ด เสร็จทุกราย
                   </h2>
                   <ul class="mt-2 space-y-1">
@@ -97,36 +97,32 @@
                       <Icon class="w-5 h-5" name="line-md:arrow-right"></Icon>
                       &#32; 16/04/2566
                     </li>
-                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-                      <Icon class="w-6 h-6" name="fluent:people-audience-24-filled"></Icon>
-                      35 คน
-                    </li>
-                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-                      <Icon class="w-6 h-6" name="fluent:comment-error-16-filled"></Icon>
-                      สนุกดี
-                    </li>
                   </ul>
                   <h2 class="text-xl font-bold dark:text-white mt-3">
-                    โรงแรม
+                    ไกด์นำเที่ยว
                   </h2>
-                  <ul class="mt-2 space-y-1">
-                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-                      <Icon class="w-6 h-6" name="ri:hotel-fill"></Icon>
-                      โรงแรมมาดี เวลา : 2016-10-15 - 2016-10-16
+                  <ol class="relative border-l border-gray-200 dark:border-gray-700">
+                    <li class=" ml-4">
+                      <div
+                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
+                      </div>
+                      <h2 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">นายสมชาย วดี</h2>
+                      <p class="mb-1 text-base font-normal text-gray-500 dark:text-gray-400">เบอร์ติดต่อ </p>
+                      <p class="text-base font-normal text-gray-900 dark:text-gray-400">092-868552 </p>
+                      <p class="mb-1 text-base font-normal text-gray-500 dark:text-gray-400">ประเภทมัคคุเทศก์ </p>
+                      <p class="mb-4 text-base font-normal text-gray-900 dark:text-gray-400">ทั่วไป/ภาคเหนือ</p>
                     </li>
-
-                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-                      <Icon class="w-6 h-6" name="ri:hotel-fill"></Icon>
-                      โรงแรมมาดี เวลา : 2016-10-15 - 2016-10-16
+                    <li class="mb-10 ml-4">
+                      <div
+                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
+                      </div>
+                      <h2 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">นาสาวมณี วันดี</h2>
+                      <p class="mb-1 text-base font-normal text-gray-500 dark:text-gray-400">เบอร์ติดต่อ </p>
+                      <p class="text-base font-normal text-gray-900 dark:text-gray-400">092-868552 </p>
+                      <p class="mb-1 text-base font-normal text-gray-500 dark:text-gray-400">ประเภทมัคคุเทศก์ </p>
+                      <p class="mb-4 text-base font-normal text-gray-900 dark:text-gray-400">ทั่วไป/ภาคเหนือ</p>
                     </li>
-                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-                      <Icon class="w-6 h-6" name="ri:hotel-fill"></Icon>
-                      โรงแรมมาดี เวลา : 2016-10-15 - 2016-10-16
-                    </li>
-                  </ul>
-                  <div class="mt-2 flex justify-end">
-                    <o-button>เสรร็จสื้น</o-button>
-                  </div>
+                  </ol>
                 </div>
               </div>
 
@@ -149,7 +145,7 @@
                     <o-field label="ที่อยู่">
                       <o-input></o-input>
                     </o-field>
-                  </div>                  
+                  </div>
                   <div>
                     <o-button size="medium" variant="primary" @click="newaddhotel = false"> บันทึก </o-button>
                     <o-button size="medium" variant="primary" @click="newaddhotel = false"> ยกเลิก </o-button>
@@ -194,9 +190,154 @@
                   </div>
                 </div>
                 <div class="flex justify-end">
-                  
-                    <o-button @click="newaddhotel=true">+ เพิ่มโรงแรม</o-button>
-                 
+                  <NuxtLink to="/addguide">
+
+                    <o-button>+ ไกด์</o-button>
+                  </NuxtLink>
+
+                </div>
+              </div>
+
+              <div>
+                <o-table :data="data" v-model:checked-rows="checkedRows" checkable :checkbox-position="checkboxPosition">
+                  <o-table-column v-for="column in columns" v-bind="column" #default="{ row }">
+                    {{ row[column.field] }}
+                  </o-table-column>
+                </o-table>
+              </div>
+            </UiCard>
+          </div>
+        </div>
+      </section>
+    </o-step-item>
+    <o-step-item step="3" label="ข้อมูลโรงแรม" :clickable="true" icon="user-plus">
+      <!-- 3 โรงแรม -->
+      <section class="h-m-screen">
+        <div class="grid grid-cols-1 xl:grid-cols-3 xl:gap-4">
+          <div class="col-span-full xl:col-auto mb-4">
+            <UiCard>
+              <div class="sm:flex xl:block sm:space-x-4 xl:space-x-0">
+
+                <div>
+                  <h2 class=" flex text-xl font-bold dark:text-white">
+                    <Icon class="w-6 h-6" name="mdi:airplane-marker"></Icon>
+                    ทัวร์เกาะเสม็ด เสร็จทุกราย
+                  </h2>
+                  <ul class="mt-2 space-y-1">
+                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                      <Icon class="w-6 h-6" name="ri:treasure-map-fill"></Icon>
+                      กาญจนบุรี-เพชรบุรี
+                    </li>
+                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                      <Icon class="w-6 h-6" name="line-md:moon-filled-to-sunny-filled-transition"></Icon>
+                      5 วัน 4 คืน
+                    </li>
+                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                      <Icon class="w-6 h-6" name="line-md:calendar"></Icon>
+                      16/04/2566 &#32;
+                      <Icon class="w-5 h-5" name="line-md:arrow-right"></Icon>
+                      &#32; 16/04/2566
+                    </li>
+                  </ul>
+                  <h2 class="text-xl font-bold dark:text-white mt-3">
+                    โรงแรม
+                  </h2>
+                  <ol class="relative border-l border-gray-200 dark:border-gray-700">
+                    <li class=" ml-4">
+                      <div
+                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
+                      </div>
+                      <h2 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">โรงแรมกล้วยไม้สีขาว</h2>
+                      <p class="mb-2 text-base font-normal text-gray-500 dark:text-gray-400">
+                        เช็คอิน 02-05-66
+                      </p>
+                      <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">เช็คเอ้า 02-05-66</p>
+                    </li>
+                    <li class="mb-10 ml-4">
+                      <div
+                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
+                      </div>
+                      <h2 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">โรงแรมกล้วยไม้สีขาว</h2>
+                      <p class="mb-2 text-base font-normal text-gray-500 dark:text-gray-400">
+                        เช็คอิน 02-05-66
+                      </p>
+                      <h2 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">โรงแรมกล้วยไม้สีขาว</h2>
+                      <p class="mb-2 text-base font-normal text-gray-500 dark:text-gray-400">
+                        เช็คอิน 02-05-66
+                      </p>
+                      <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">เช็คเอ้า 02-05-66</p>
+                    </li>
+                  </ol>
+                </div>
+              </div>
+
+              <o-modal v-model:active="newaddhotel">
+                <div class=" grid grid-cols-2 gap-4 p-6">
+                  <div class="col-span-2">
+                    <label>เพิ่มโรงแรมใหม่</label>
+                  </div>
+                  <div>
+                    <o-field label="ชื่อโรงแรม">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div>
+                    <o-field label="เบอร์ติดต่อ">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div class="col-span-2">
+                    <o-field label="ที่อยู่">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div>
+                    <o-button size="medium" variant="primary" @click="newaddhotel = false"> บันทึก </o-button>
+                    <o-button size="medium" variant="primary" @click="newaddhotel = false"> ยกเลิก </o-button>
+                  </div>
+                </div>
+              </o-modal>
+              <o-modal v-model:active="addhotel">
+                <div class="grid grid-cols-2 gap-4 p-6">
+                  <div class="col-span-2">
+                    <label>เลือกวันเข้าพัก</label>
+                  </div>
+                  <div>
+                    <o-field label="วันเข้าพัก">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div>
+                    <o-field label="วันสิ้นสุด">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div class="col-span-2">
+                    <o-field label="จำนวนห้องพัก">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div>
+                    <o-button size="medium" variant="primary" @click="addhotel = false"> บันทึก </o-button>
+                    <o-button size="medium" variant="primary" @click="addhotel = false"> ยกเลิก </o-button>
+                  </div>
+                </div>
+              </o-modal>
+            </UiCard>
+          </div>
+          <div class="col-span-2">
+            <UiCard>
+              <div class="my-2 grid grid-cols-2 gap-2 mt-5">
+                <div class="flex">
+                  <o-input placeholder="ค้นหาลูกทัวร์"></o-input>
+                  <div class="px-2">
+                    <o-button>ค้นหา</o-button>
+                  </div>
+                </div>
+                <div class="flex justify-end">
+
+                  <o-button @click="newaddhotel = true">+ เพิ่มโรงแรม</o-button>
+
                 </div>
               </div>
 
@@ -210,7 +351,142 @@
         </div>
       </section>
     </o-step-item>
-    <o-step-item step="4" label="ข้อมูลพหนะ" :clickable="true" icon="user-plus"></o-step-item>
+    <o-step-item step="4" label="ข้อมูลพหนะ" :clickable="true" icon="user-plus">
+      <!-- 4 พาหนะ -->
+      <section class="h-m-screen">
+        <div class="grid grid-cols-1 xl:grid-cols-3 xl:gap-4">
+          <div class="col-span-full xl:col-auto mb-4">
+            <UiCard>
+              <div class="sm:flex xl:block sm:space-x-4 xl:space-x-0">
+
+                <div>
+                  <h2 class="text-xl font-bold dark:text-white">
+                    <Icon class="w-6 h-6" name="mdi:airplane-marker"></Icon>
+                    ทัวร์เกาะเสม็ด เสร็จทุกราย
+                  </h2>
+                  <ul class="mt-2 space-y-1">
+                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                      <Icon class="w-6 h-6" name="ri:treasure-map-fill"></Icon>
+                      กาญจนบุรี-เพชรบุรี
+                    </li>
+                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                      <Icon class="w-6 h-6" name="line-md:moon-filled-to-sunny-filled-transition"></Icon>
+                      5 วัน 4 คืน
+                    </li>
+                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                      <Icon class="w-6 h-6" name="line-md:calendar"></Icon>
+                      16/04/2566 &#32;
+                      <Icon class="w-5 h-5" name="line-md:arrow-right"></Icon>
+                      &#32; 16/04/2566
+                    </li>
+                  </ul>
+                  <h2 class="text-xl font-bold dark:text-white mt-3">
+                    พาหนะ
+                  </h2>
+                  <ol class="relative border-l border-gray-200 dark:border-gray-700">
+                    <li class=" ml-4">
+                      <div
+                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
+                      </div>
+                      <h2 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">รถบัสVIP</h2>
+                      <p class="mb-1 text-base font-normal text-gray-500 dark:text-gray-400">จำนวน </p>
+                      <p class="mb-1 text-base font-normal text-gray-900 dark:text-gray-400">2 </p>
+                      <p class="mb-1 text-base font-normal text-gray-500 dark:text-gray-400">เบอร์ติดต่อ </p>
+                      <p class="mb-2 text-base font-normal text-gray-900 dark:text-gray-400">092-868552 </p>
+                      <p class="mb-4 text-base font-normal text-gray-900 dark:text-gray-400">ขาไปและขากลับ</p>
+                    </li>
+                    <li class="mb-10 ml-4">
+                      <div
+                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
+                      </div>
+                      <h2 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">นกแอร์</h2>
+                      <p class="mb-1 text-base font-normal text-gray-500 dark:text-gray-400">จำนวน </p>
+                      <p class="mb-1 text-base font-normal text-gray-900 dark:text-gray-400">30 </p>
+                      <p class="mb-1 text-base font-normal text-gray-500 dark:text-gray-400">เบอร์ติดต่อ </p>
+                      <p class="mb-2 text-base font-normal text-gray-900 dark:text-gray-400">092-868552 </p>
+                      <p class="mb-4 text-base font-normal text-gray-900 dark:text-gray-400">ขาไป</p>
+                    </li>
+                  </ol>
+                </div>
+              </div>
+
+              <o-modal v-model:active="newaddhotel">
+                <div class=" grid grid-cols-2 gap-4 p-6">
+                  <div class="col-span-2">
+                    <label>เพิ่มโรงแรมใหม่</label>
+                  </div>
+                  <div>
+                    <o-field label="ชื่อโรงแรม">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div>
+                    <o-field label="เบอร์ติดต่อ">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div class="col-span-2">
+                    <o-field label="ที่อยู่">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div>
+                    <o-button size="medium" variant="primary" @click="newaddhotel = false"> บันทึก </o-button>
+                    <o-button size="medium" variant="primary" @click="newaddhotel = false"> ยกเลิก </o-button>
+                  </div>
+                </div>
+              </o-modal>
+              <o-modal v-model:active="newvehicle">
+                <div class="grid grid-cols-2 gap-4 p-6">
+                  <div class="col-span-2">
+                    <label>เพิ่มพาหนะ</label>
+                  </div>
+                  <div>
+                    <o-field label="พาหนะ">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div>
+                    <o-field label="เลขพาหนะ">
+                      <o-input></o-input>
+                    </o-field>
+                  </div>
+                  <div>
+                    <o-button size="medium" variant="primary" @click="newvehicle = false"> บันทึก </o-button>
+                    <o-button size="medium" variant="primary" @click="newvehicle = false"> ยกเลิก </o-button>
+                  </div>
+                </div>
+              </o-modal>
+            </UiCard>
+          </div>
+          <div class="col-span-2">
+            <UiCard>
+              <div class="my-2 grid grid-cols-2 gap-2 mt-5">
+                <div class="flex">
+                  <o-input placeholder="ค้นหาลูกทัวร์"></o-input>
+                  <div class="px-2">
+                    <o-button>ค้นหา</o-button>
+                  </div>
+                </div>
+                <div class="flex justify-end">
+                    <o-button @click="newvehicle =true">+ เพิ่มพาหนะ</o-button>
+
+
+                </div>
+              </div>
+
+              <div>
+                <o-table :data="data" v-model:checked-rows="checkedRows" checkable :checkbox-position="checkboxPosition">
+                  <o-table-column v-for="column in columns" v-bind="column" #default="{ row }">
+                    {{ row[column.field] }}
+                  </o-table-column>
+                </o-table>
+              </div>
+            </UiCard>
+          </div>
+        </div>
+      </section>
+    </o-step-item>
   </o-steps>
 </template>
 <script setup lang="ts">
@@ -220,6 +496,7 @@ onMounted(() => { initFlowbite() });
 
 const addhotel = ref(false);
 const newaddhotel = ref(false);
+const newvehicle = ref(false);
 
 
 const columns = ref([
