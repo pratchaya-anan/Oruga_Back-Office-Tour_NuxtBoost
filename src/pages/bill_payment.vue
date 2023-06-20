4db88283c4e1e861f6a073d6e010f00a8f3cb0e9
jane99j <jaynerrch@gmail.com>
2023-06-20 14:36:44 +0700

    clear&payment

--- src/pages/bill_payment.vue
+++ src/pages/bill_payment.vue
@@ -137,7 +137,8 @@
                 <div class="col-span-2">
                     <div class=" overflow-x-auto shadow-md sm:rounded-lg mb-4">
 
-                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <UiCard>
+                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th scope="col" class="px-6 py-3">
@@ -201,6 +202,7 @@
                                 </tr>
                             </tbody>
                         </table>
+                        </UiCard>
 
                         <!-- Dropdown menu -->
                         <div id="dropdownDots"
