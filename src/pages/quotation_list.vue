5be2ee2aaac046eeaf4d936de4c9f87abc082f33
OB02CGTH <sornsiri1208@gmail.com>
2023-06-20 13:31:32 +0700

    Succes Billing abd Taxinvoice

--- src/pages/quotation_list.vue
+++ src/pages/quotation_list.vue
@@ -161,7 +161,7 @@
                                     </diV>
                                     <div class="flex justify-end">
                                         <!-- <NuxtLink to="/paper/billing_paper"> -->
-                                        <a href="/taxinvoice_create">
+                                        <a href="/billing_create">
                                             <o-button
                                                 class="mr-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 <Icon class="mr-3 -ml-1 w-5 h-5" name="icon-park-solid:bill"></Icon>
@@ -170,7 +170,7 @@
                                         </a>
                                         <!-- </NuxtLink> -->
                                         <!-- <NuxtLink to="/paper/taxinvoice_paper"> -->
-                                        <a href="/paper/taxinvoice_paper">
+                                        <a href="/taxinvoice_create">
                                             <o-button
                                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 <Icon class="mr-3 -ml-1 w-5 h-5" name="fa6-solid:file-invoice-dollar">
