5421a37f30dc8d97ecdef7fd7251a654024e2a2b
pratchaya-anan <pratchaya-anan.sr.62@ubu.ac.th>
2023-06-24 09:18:16 +0700

    update

--- src/pages/billing_list.vue
+++ src/pages/billing_list.vue
@@ -4,7 +4,7 @@
 
     <UiManageBar>
       <NuxtLink
-        to="billing_create"
+        to="/billing_create"
         type="button"
         class="text-white bg-blue-700 justify-center hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 inline-flex items-center"
       >
