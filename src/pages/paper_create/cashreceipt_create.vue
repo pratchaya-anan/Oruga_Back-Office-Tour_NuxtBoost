44172171e9ee8384887c8e18895b79364c7df169
OB02CGTH <sornsiri1208@gmail.com>
2023-06-24 17:40:49 +0700

    Update  cashreceipt_create

--- src/pages/paper_create/cashreceipt_create.vue
+++ src/pages/paper_create/cashreceipt_create.vue
@@ -35,27 +35,22 @@
           </div>
           <div class="grid grid-cols-2 gap-4 mt-3">
             <div>
-              <o-field label="ชื่อลูกค้า">
+              <o-field label="ชื่อ-นามสกุล">
                 <o-input></o-input>
               </o-field>
             </div>
             <div>
-              <o-field label="เลขประจำตัวผู้เสียภาษี">
+              <o-field label="ตำแหน่ง">
                 <o-input></o-input>
               </o-field>
             </div>
             <div>
-              <o-field label="ชื่อผู้ติดต่อ">
+              <o-field label="เลขประจำตัวบัตรประชาชน">
                 <o-input></o-input>
               </o-field>
             </div>
             <div>
-              <o-field label="เบอร์โทรศัพท์">
-                <o-input></o-input>
-              </o-field>
-            </div>
-            <div>
-              <o-field label="อีเมล">
+              <o-field label="เบอร์โทร">
                 <o-input></o-input>
               </o-field>
             </div>
@@ -64,22 +59,26 @@
                 <o-input></o-input>
               </o-field>
             </div>
-          </div>
-        </div>
-      </UiCard>
-      <UiCard class="mb-3">
-        <div class="p-5">
-          <div>
-            <label class="text-2xl font-medium">ข้อมูลฝ่ายขาย</label>
-          </div>
-          <div class="grid grid-cols-2 gap-4">
-            <div class="col-span-2">
-              <o-field label="รหัสลูกค้า">
+            <div>
+              <o-field label="วันที่">
                 <o-input></o-input>
               </o-field>
             </div>
+            <div class="grid grid-cols-2">
+              <o-field label="ประเภทการจ่าย">
+                <div class="flex mt-3">
+                  <o-radio v-model="showcheck" name="name" native-value="persen" class="mr-3">
+                    จ่ายบุคคลภายใน
+                  </o-radio>
+                  <o-radio v-model="showcheck" name="name" native-value="custom">
+                    จ่ายบุคคลภายนอก
+                  </o-radio>
+                </div>
+              </o-field>
+            </div>
+
             <div>
-              <o-field label="ยืนยันราคาภายใน">
+              <o-field label="ตั้งแต่วันที">
                 <div class="relative ">
                   <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
@@ -96,7 +95,7 @@
               </o-field>
             </div>
             <div>
-              <o-field label="วันที่ส่งของ">
+              <o-field label="ถึงวันที่">
                 <div class="relative ">
                   <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
@@ -110,82 +109,17 @@
                     class=" border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Select date">
                 </div>
-              </o-field>
-            </div>
-            <div>
-              <o-field label="ผู้เสนอขาย">
-                <o-input></o-input>
-              </o-field>
-            </div>
-            <div>
-              <div>
-                <o-field label="ฝ่ายขาย">
-                  <o-select placeholder="การตลาด">
-                    <option value="free">การตลาด</option>
-                    <option value="1%">xxxx</option>
-                    <option value="3%">xxxx</option>
-                  </o-select>
-                </o-field>
-              </div>
-            </div>
-          </div>
-          <div class="grid grid-cols-2">
-            <o-field label="กำหนดราคามัดจำ">
-              <div class="flex mt-3">
-                <o-radio v-model="showcheck" name="name" native-value="persen" class="mr-3">
-                  เปอร์เซ็น
-                </o-radio>
-                <o-radio v-model="showcheck" name="name" native-value="custom">
-                  จำนวนเงิน
-                </o-radio>
-              </div>
-            </o-field>
-            <section class="my-4 ">
-              <o-field v-if="showcheck == 'persen'">
-                <o-input modelValue=""></o-input>
-              </o-field>
-              <o-field v-if="showcheck == 'custom'">
-                <o-input modelValue=""></o-input>
-              </o-field>
-              <!-- <section>
-                              <o-button @click="showcheck = false" class="mt-4">Save</o-button>
-                          </section> -->
-            </section>
-          </div>
-          <div class="grid grid-cols-2 gap-4 mt-3">
-            <div>
-              <o-field label="วันที่ออกใบ">
-                <div class="relative ">
-                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                      viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd"
-                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                        clip-rule="evenodd"></path>
-                    </svg>
-                  </div>
-                  <input datepicker type="text"
-                    class=" border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Select date">
-                </div>
-              </o-field>
-            </div>
-            <div class="flex space-x-5">
-              <o-radio v-model="branch" name="branchcheck" native-value="MainBranch">
-                สำนักงานใหญ่
-              </o-radio>
-              <o-radio v-model="branch" name="branchcheck" native-value="SubBranch">
-                สาขาย่อย
-              </o-radio>
-            </div>
-            <div v-if="branch == 'SubBranch'">
-              <o-field label="ชื่อสาขาย่อย">
-                <o-input></o-input>
               </o-field>
             </div>
           </div>
         </div>
       </UiCard>
+
+      <div class="flex justify-end">
+        <NuxtLink to="/paper_edit/cashreceipt_item">
+          <o-button>บันทึก</o-button>
+        </NuxtLink>
+      </div>
     </o-step-item>
 
 
