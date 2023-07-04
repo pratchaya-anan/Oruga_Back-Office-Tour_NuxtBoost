feabc76a0225d00e822c190ca7feaa214fa836f3
OB02CGTH <sornsiri1208@gmail.com>
2023-07-04 13:18:16 +0700

    delete loanrequest

---
+++ src/pages/coop/loanrequest_1.vue
@@ -0,0 +1,303 @@
+<template>
+  <section class="min-h-screen">
+      <div class="grid grid-cols-1 xl:grid-cols-3 xl:gap-4">
+        <div class="col-span-full xl:col-auto mb-4">
+          <UiCard class="mb-4">
+            <section>
+              <div class="sm:flex xl:block sm:space-x-4 xl:space-x-0">
+                <div>
+                  <div class="grid grid-cols-6">
+                    <div class="col-span-5">
+                      <h2 class="text-xl font-bold dark:text-white">
+                        ข้อมูลหน่วยงาน / ลูกค้า
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="border-t border-gray-200 dark:border-gray-700">
+              <div class="sm:flex xl:block xl:space-y-4">
+                <div class="sm:flex-1">
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-4">หน่วยงาน / ลูกค้า</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      เทศบาลขอนแก่น
+                    </div>
+                  </div>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">รหัสลูกค้า</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      1552555214
+                    </div>
+                    <div class="mt-2">ชื่อผู้ติดต่อ</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      นายภาคภูมิ ภูเขียว
+                    </div>
+                    <div class="mt-2">เบอร์ติดต่อ</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">0625584785
+                    </div>
+                  </div>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">เลขประจำตัวผู้เสียภาษี</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      1522236
+                    </div>
+                  </div>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">ที่อยู่</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      175 ม.12 ต.บ้านเป็ด อ.เมืองขอนแก่น จ.ขอนแก่น
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </UiCard>
+          <UiCard class="mb-4">
+            <section>
+              <div class="sm:flex xl:block sm:space-x-4 xl:space-x-0">
+                <div>
+                  <div class="grid grid-cols-6">
+                    <div class="col-span-5">
+                      <h2 class="text-xl font-bold dark:text-white">
+                        ข้อมูลทัวร์
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="border-t border-gray-200 dark:border-gray-700">
+              <div class="sm:flex xl:block xl:space-y-4">
+                <div class="sm:flex-1">
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-4">ชื่อทริปทัวร์</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      เที่ยวเกาะเลาะรัก
+                    </div>
+                  </div>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">ชื่อโปรแกรมทัวร์</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      กาญจนบุรี-พัทยา
+                    </div>
+                  </div>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">วันที่เดินทาง</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      05/06/2566
+                    </div>
+                  </div>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">วันที่สิ้นสุด</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      07/06/2566
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </UiCard>
+          <UiCard class="mb-4">
+            <section>
+              <div class="sm:flex xl:block sm:space-x-4 xl:space-x-0">
+                <div>
+                  <div class="grid grid-cols-6">
+                    <div class="col-span-5">
+                      <h2 class="text-xl font-bold dark:text-white">
+                        ใบวางบิล
+                      </h2>
+                    </div>
+                  </div>
+                  <ul class="mt-2 space-y-1">
+                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                      Billing Note/Invoice
+                    </li>
+                    <li class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                      เลขที่: B230430-0001
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </section>
+            <section class="my-4 border-t border-gray-200 dark:border-gray-700">
+              <span>รายละเอียดใบเสนอราคา</span>
+              <div class="sm:flex xl:block xl:space-y-4">
+                <div class="sm:flex-1">
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                      <div class="mt-4">หน่วยงาน / ลูกค้า</div>
+                      <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        เทศบาลขอนแก่น
+                      </div>
+                    </div>
+                    <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                      <div class="mt-2">รหัสลูกค้า</div>
+                      <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        1552555214
+                      </div>
+                      <div class="mt-2">ชื่อผู้ติดต่อ</div>
+                      <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        นายภาคภูมิ ภูเขียว
+                      </div>
+                      <div class="mt-2">เบอร์ติดต่อ</div>
+                      <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">0625584785
+                      </div>
+                    </div>
+                    <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                      <div class="mt-2">เลขประจำตัวผู้เสียภาษี</div>
+                      <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        1522236
+                      </div>
+                    </div>
+                    <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                      <div class="mt-2">Email</div>
+                      <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        somchai@gmail.com
+                      </div>
+                    </div>
+                    <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                      <div class="mt-2">ที่อยู่</div>
+                      <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        175 ม.12 ต.บ้านเป็ด อ.เมืองขอนแก่น จ.ขอนแก่น
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="sm:flex-1">
+                  <div class="my-4 border-t border-gray-200 dark:border-gray-700"></div>
+                  <span>รายละเอียดฝ่ายขาย</span>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">ผู้เสนอขาย</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      สมชัย ใจเกินร้อย
+                    </div>
+                  </div>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">ฝ่าย:</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      ฝ่ายขาย
+                    </div>
+                  </div>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">ยืนยันราคาภายในวันที่</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      16/07/2566
+                    </div>
+                  </div>
+                  <div class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400">
+                    <div class="mt-2">วันส่งของ:</div>
+                    <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      17/07/2566
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </UiCard>
+        </div>
+        <div class="col-span-2">
+          <UiCard class="mb-3">
+            <div class="p-5">
+              <div class="mb-2">
+                <h2 class="text-xl font-bold dark:text-white">ข้อมูลเอกสาร</h2>
+              </div>
+              <div class="grid grid-cols-2 gap-4">
+                <div>
+                  <o-field label="ผู้ขาย">
+                    <o-select placeholder="เลือกผู้ขาย">
+                      <option value="นายกอไก่ ขอไข่">นายกอไก่ ขอไข่</option>
+                      <option value="นายคอควาย งองู">นายคอควาย งองู</option>
+                      <option value="นางสวยสด งดงาม">นางสวยสด งดงาม</option>
+                    </o-select>
+                  </o-field>
+                </div>
+                <div>
+                  <o-field label="ฝ่าย">
+                    <o-select placeholder="เลือกฝ่าย">
+                      <option value="บัญชี">บัญชี</option>
+                      <option value="การเงิน">การเงิน</option>
+                      <option value="??">??</option>
+                    </o-select>
+                  </o-field>
+                </div>
+                <div>
+                  <o-field label="วันที่ออกใบ">
+                    <div class="relative">
+                      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd"
+                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                            clip-rule="evenodd"></path>
+                        </svg>
+                      </div>
+                      <input datepicker type="text"
+                        class="border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Select date" />
+                    </div>
+                  </o-field>
+                </div>
+                <div>
+                  <o-field label="ยืนยันราคาวันที่">
+                    <div class="relative">
+                      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd"
+                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                            clip-rule="evenodd"></path>
+                        </svg>
+                      </div>
+                      <input datepicker type="text"
+                        class="border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Select date" />
+                    </div>
+                  </o-field>
+                </div>
+                <div>
+                  <o-field label="กำหนดชำระ">
+                    <div class="relative">
+                      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd"
+                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                            clip-rule="evenodd"></path>
+                        </svg>
+                      </div>
+                      <input datepicker type="text"
+                        class="border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Select date" />
+                    </div>
+                  </o-field>
+                </div>
+                <div>
+                  <o-field label="ประเภทสาขา">
+                    <div class="flex mt-3">
+                      <o-radio v-model="branch" name="branchcheck" native-value="MainBranch" class="mr-3">
+                        สำนักงานใหญ่
+                      </o-radio>
+                      <o-radio v-model="branch" name="branchcheck" native-value="SubBranch">
+                        สาขาย่อย
+                      </o-radio>
+                    </div>
+                    <o-input v-if="branch == 'SubBranch'"></o-input>
+                  </o-field>
+                </div>
+              </div>
+              <div class="grid grid-cols-2 gap-4 mt-3">
+
+              </div>
+            </div>
+            <div class="flex justify-end">
+              <a href="/paper_edit/taxinvoice_item">
+                <o-button> บันทึก </o-button>
+              </a>
+            </div>
+          </UiCard>
+        </div>
+      </div>
+    </section>
+  </template>
