fd07660434057c5c4a794a7fc2aa03679400d029
pratchaya-anan <pratchaya-anan.sr.62@ubu.ac.th>
2023-07-04 15:05:59 +0700

    add page securities

---
+++ src/pages/coop/loanrequest_securities.vue
@@ -0,0 +1,270 @@
+<template>
+  <section class="min-h-screen">
+    <LayoutPageTitle>ตรวจสอบเอกสารผู้กู้</LayoutPageTitle>
+    <div class="grid grid-cols-1 xl:grid-cols-3 xl:gap-4">
+      <div class="col-span-full xl:col-auto mb-4">
+        <UiCard class="mb-4">
+          <section>
+            <div class="sm:flex xl:block sm:space-x-4 xl:space-x-0">
+              <div class="grid grid-cols-6">
+                <div class="col-span-5">
+                  <h2 class="text-xl font-bold dark:text-white">
+                    คำขอกู้เงินสามัญทั่วไป
+                  </h2>
+                </div>
+              </div>
+              <ul class="mt-2 space-y-1">
+                <li
+                  class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400"
+                >
+                  เขียนที่
+                </li>
+                <li
+                  class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400"
+                >
+                  วันที่
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section class="my-4 border-t border-gray-200 dark:border-gray-700">
+            <span>ข้อมูลผู้กู้</span>
+            <div class="sm:flex xl:block xl:space-y-4">
+              <div class="sm:flex-1">
+                <div
+                  class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                >
+                  <div
+                    class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                  >
+                    <div class="mt-4">ชื่อ-สกุล</div>
+                    <div
+                      class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      นายภาคภูมิ ภูเขียว
+                    </div>
+                  </div>
+                  <div
+                    class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                  >
+                    <div class="mt-4">เลขทะเบียนสมาชิก</div>
+                    <div
+                      class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      1552555214
+                    </div>
+                  </div>
+                  <div
+                    class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                  >
+                    <div class="mt-4">อายุ</div>
+                    <div
+                      class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      30
+                    </div>
+                  </div>
+                  <div
+                    class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                  >
+                    <div class="mt-2">หน้าที่</div>
+                    <div
+                      class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      พนักงานราชการ
+                    </div>
+                  </div>
+                  <div
+                    class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                  >
+                    <div class="mt-2">ตำแหน่ง</div>
+                    <div
+                      class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      -----
+                    </div>
+                  </div>
+                  <div
+                    class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                  >
+                    <div class="mt-2">สังกัด</div>
+                    <div
+                      class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      -----
+                    </div>
+                  </div>
+                  <div
+                    class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                  >
+                    <div class="mt-2">ที่อยู่</div>
+                    <div
+                      class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      175 ม.12 ต.บ้านเป็ด อ.เมืองขอนแก่น จ.ขอนแก่น 40000
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="sm:flex-1 border-t pt-2">
+                <span>ข้อมูลคู่สมรส</span>
+                <div
+                  class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                >
+                  <div class="mt-4">ชื่อ-สกุล</div>
+                  <div
+                    class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    นายภาคภูมิ ภูเขียว
+                  </div>
+                </div>
+                <div
+                  class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                >
+                  <div class="mt-4">อายุ</div>
+                  <div
+                    class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    30
+                  </div>
+                </div>
+                <div
+                  class="text-sm not-italic font-normal text-gray-500 dark:text-gray-400"
+                >
+                  <div class="mt-2">ที่อยู่</div>
+                  <div
+                    class="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    175 ม.12 ต.บ้านเป็ด อ.เมืองขอนแก่น จ.ขอนแก่น 40000
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </UiCard>
+      </div>
+
+      <!-- รายการเอกสาร -->
+      <div class="col-span-2 gap-4">
+        <div class="flex flex-wrap gap-4">
+          <UiCard class="flex-1">
+            <div class="my-2">
+              <h2 class="text-xl font-bold dark:text-white">เอกสารที่แนบมา</h2>
+              <span>เอกสารผู้กู้</span>
+            </div>
+            <div class="space-y-2">
+              <UiCard class="flex justify-between"
+                ><div>สำเนาบัตรประชาชน</div>
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >ตรวจสอบ</a
+                >
+              </UiCard>
+              <UiCard class="flex justify-between"
+                ><div>สำเนาบัตรทะเบียนบ้าน</div>
+                <a
+                  href="#"
+                  class="font-medium text-gray-500 dark:text-blue-500 hover:underline"
+                  >ไม่มีไฟล์แนบ</a
+                ></UiCard
+              >
+              <UiCard class="flex justify-between"
+                ><div>สลิปเงินเดือน</div>
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >ตรวจสอบ</a
+                ></UiCard
+              >
+              <UiCard class="flex justify-between"
+                ><div>สำเนาทะเบียนสมรส</div>
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >ตรวจสอบ</a
+                ></UiCard
+              >
+              <UiCard class="flex justify-between">
+                <div>สำเนาใบเปลี่ยนชื่อ</div>
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >ตรวจสอบ</a
+                ></UiCard
+              >
+            </div>
+            <div class="my-2">
+              <span>เอกสารคู่สมรส</span>
+            </div>
+            <div class="space-y-2">
+              <UiCard class="flex justify-between"
+                ><div>สำเนาบัตรประชาชน</div>
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >ตรวจสอบ</a
+                >
+              </UiCard>
+              <UiCard class="flex justify-between"
+                ><div>สำเนาบัตรทะเบียนบ้าน</div>
+                <a
+                  href="#"
+                  class="font-medium text-gray-500 dark:text-blue-500 hover:underline"
+                  >ไม่มีไฟล์แนบ</a
+                ></UiCard
+              >
+              <UiCard class="flex justify-between"
+                ><div>สำเนาทะเบียนสมรส</div>
+                <a
+                  href="#"
+                  class="font-medium text-gray-500 dark:text-blue-500 hover:underline"
+                  >ไม่มีไฟล์แนบ</a
+                ></UiCard
+              >
+              <UiCard class="flex justify-between">
+                <div>สำเนาใบเปลี่ยนชื่อ</div>
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >ตรวจสอบ</a
+                ></UiCard
+              >
+            </div>
+            <div class="grid grid-cols-2 w-full mt-2">
+              <o-field label="เอกสารที่มี">
+                <div class="flex space-x-5 mt-2">
+                  <o-radio native-value="เอกสารถูกต้อง" name="check"
+                    >เอกสารถูกต้อง</o-radio
+                  >
+                  <o-radio native-value="เอกสารไม่ถูกต้อง" name="check"
+                    >เอกสารไม่ถูกต้อง</o-radio
+                  >
+                </div>
+              </o-field>
+              <o-field label="คอมเมนต์">
+                <o-input></o-input>
+              </o-field>
+            </div>
+          </UiCard>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-end space-x-5 mt-5">
+      <a href="#">
+        <o-button> บันทึก </o-button>
+      </a>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { initFlowbite } from "flowbite";
+
+const showItemsPanel = ref(false);
+const showItemsListComment = ref(false);
+
+onMounted(() => {
+  initFlowbite();
+});
+</script>
